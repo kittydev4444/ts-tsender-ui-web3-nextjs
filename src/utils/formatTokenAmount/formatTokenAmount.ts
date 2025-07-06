6b17892c0d1c11166e5bacb1a498029edfca0206
Foursqweez <jeerasak.code@gmail.com>
2025-07-06 15:20:41 +0700

    Update formatTokenAmount to return 0.00 instead of NaN

--- src/utils/formatTokenAmount/formatTokenAmount.ts
+++ src/utils/formatTokenAmount/formatTokenAmount.ts
@@ -1,7 +1,9 @@
 export function formatTokenAmount(weiAmount: number, decimals: number): string {
   const tokenAmount = weiAmount / Math.pow(10, decimals)
-  return tokenAmount.toLocaleString(undefined, {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  })
+  return tokenAmount
+    ? tokenAmount.toLocaleString(undefined, {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      })
+    : "0.00"
 }
